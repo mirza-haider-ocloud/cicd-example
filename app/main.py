b1d2b317c2edf5be87cb7f6f198779ec6d8443c3
mac <mac@macs-MacBook-Pro.local>
2025-05-19 12:52:10 +0500

    Have a good day

--- app/main.py
+++ app/main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "Bingo"}
 
+@app.get("/good-day")
+def good_day():
+    return {"Hi": "Have a good day !!"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
