5a2d789ee97330f8a2a40cf8eab76f8628127298
mirza-haider-ocloud <mirza.haider@ocloudsolutions.net>
2025-05-26 13:08:11 +0500

    allow https

--- infra/main.tf
+++ infra/main.tf
@@ -69,6 +69,13 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     description = "SSH from anywhere"
     from_port   = 22
     to_port     = 22
